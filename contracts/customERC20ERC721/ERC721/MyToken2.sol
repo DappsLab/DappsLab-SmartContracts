55d1535f1879fdcac5f8f87520fc05a1b8bfe890
qasimraheem <qasimmehmood13936@gmail.com>
2021-06-28 15:50:34 +0500

    compliation done

---
+++ contracts/customERC20ERC721/ERC721/MyToken2.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ERC721.sol";
+
+contract MyToken2 is ERC721 {
+    string[] internal tokens;
+
+    constructor()
+    ERC721(
+        "My Token2",
+        "TKN2"
+    ){}
+
+    function _creatorOf(uint256 tokenId) public view returns(address){
+        return _creator[tokenId];
+    }
+
+    function MintNFT(string memory URI, uint256 tokenAmount) public returns(uint256) {
+        tokens.push(URI);
+        uint256 tokenId = tokens.length - 1;
+        _safeMint(msg.sender, tokenId, tokenAmount);
+        return tokenId;
+    }
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override(ERC721) {
+        super._beforeTokenTransfer(from, to, tokenId);
+        uint256 secondaryShare = msg.value / 100 * 10;
+        payable(_creatorOf(tokenId)).transfer(secondaryShare);
+    }
+}
