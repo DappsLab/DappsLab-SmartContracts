d66ba65cf959b78bcd4f5a995342c0bee3b032eb
qasim raheem 🚀 <qasimmehmood13936@gmail.com>
2022-06-17 17:15:25 +0500

    working on ipfs

--- smartcontractInterface/src/components/ipfs.js
+++ smartcontractInterface/src/components/ipfs.js
@@ -6,23 +6,25 @@ console.log("ipfs", ipfs);
 
 export default function IPFSForm() {
 
-
+    // QmRtcvpnZdyUigMBcJLNHjDxnawBv5Cd9CbxRyNHy9SkrW
+    // QmcexpHnCcAupjpnCmGFNYXN9WRbUb4DtYumdDVE5pJTSj
+    // QmXSNdWa5FGmPqTrP3uGGXobuv8Bzb5NyzVHb2maCUmTYS
     const [state, setState] = useState({});
 
     async function uploadToIpfs() {
         //article content to ipfs as .txt file
-        // let article = state.value;
-        // const { cid } = await ipfs.add(article)
+        let article = state.value;
+        const { cid } = await ipfs.add(article)
         // await ipfs.files.mkdir('/destination-directory')
         // let folderStatus = await ipfs.files.stat('/example')
-        // await ipfs.files.write('/example/article.txt',article,{ create: true })
+        await ipfs.files.write('/example/article3.txt',article,{ create: true })
         // console.log('CID', cid);
         // console.log('folderStatus', folderStatus);
-        let ls = await ipfs.files.ls('/example')
-        let read = await ipfs.files.read('/article')
+        // let ls = await ipfs.files.ls('/example')
+        // let read = await ipfs.files.read('/article')
         // await ipfs.files.cp('/example/article.txt', '/destination-directory/article.txt', {parents: true})
-        console.log('all articles', ls);
-        console.log('read', read);
+        // console.log('all articles', ls);
+        // console.log('read', read);
     }
 
     function getFromIpfs(CID) {
