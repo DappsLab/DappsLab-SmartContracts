a3db6c5fd0283a389456a42e07fedbf8eeae63a1
qasim raheem 🚀 <qasimmehmood13936@gmail.com>
2022-06-17 19:00:20 +0500

    get ipfs

--- smartcontractInterface/src/components/ipfs.js
+++ smartcontractInterface/src/components/ipfs.js
@@ -1,90 +1,106 @@
-import React, { useState } from 'react'
-import { useRef } from 'react'
-import { create } from 'ipfs-http-client'
+import React, {useState} from 'react'
+import {useRef} from 'react'
+import {create} from 'ipfs-http-client'
 
+const ipfsGateway = 'http://127.0.0.1:8080/ipfs/';
 const ipfs = create('http://localhost:5001')
 console.log('ipfs', ipfs)
 
 export default function IPFSForm() {
-  const inputRef = useRef(null)
-  const [state, setState] = useState({})
+    const inputRef = useRef(null)
+    const [state, setState] = useState({})
 
-  const handleClick = () => {
-    // 👇️ open file input box on click of other element
-    inputRef.current.click()
-  }
+    const handleClick = () => {
+        // 👇️ open file input box on click of other element
+        inputRef.current.click()
+    }
+    const getArticle = async () => {
+        let cid = ipfsGateway+state.value;
+        setState({cid})
 
-  const handleFileChange = async (event) => {
-    const fileObj = event.target.files && event.target.files[0]
-    if (!fileObj) {
-      return
+
     }
 
-    console.log('fileObj is', fileObj)
-    console.log('file name is', fileObj.name)
+    const handleFileChange = async (event) => {
+        const fileObj = event.target.files && event.target.files[0]
+        if (!fileObj) {
+            return
+        }
 
-    // await ipfs.files.write(`/example/hello.txt`, "hello", {
-    //   create: true,
-    // })
-    await ipfs.files.write('/example/'+fileObj.name,fileObj,{ create: true })
-    // 👇️ reset file input
-    event.target.value = null
+        console.log('fileObj is', fileObj)
+        console.log('file name is', fileObj.name)
 
-    // 👇️ is now empty
-    console.log(event.target.files)
+        // await ipfs.files.write(`/example/hello.txt`, "hello", {
+        //   create: true,
+        // })
+        await ipfs.files.write('/example/' + fileObj.name, fileObj, {create: true})
+        // 👇️ reset file input
+        event.target.value = null
 
-    // 👇️ can still access file object here
-    console.log(fileObj)
-    console.log(fileObj.name)
-  }
+        // 👇️ is now empty
+        console.log(event.target.files)
 
-  async function uploadToIpfs() {
-    //article content to ipfs as .txt file
-    // let article = state.value;
-    // const { cid } = await ipfs.add(article)
-    // await ipfs.files.mkdir('/destination-directory')
-    // let folderStatus = await ipfs.files.stat('/example')
-    // await ipfs.files.write('/example/article.txt',article,{ create: true })
-    // console.log('CID', cid);
-    // console.log('folderStatus', folderStatus);
-    let ls = await ipfs.files.ls('/example')
-    let read = await ipfs.files.read('/article')
-    // await ipfs.files.cp('/example/article.txt', '/destination-directory/article.txt', {parents: true})
-    console.log('all articles', ls)
-    console.log('read', read)
-  }
+        // 👇️ can still access file object here
+        console.log(fileObj)
+        console.log(fileObj.name)
+    }
 
-  function getFromIpfs(CID) {
-    //CID string
-  }
+    async function uploadToIpfs() {
+        //article content to ipfs as .txt file
+        // let article = state.article;
+        // const { cid } = await ipfs.add(article)
+        // await ipfs.files.mkdir('/destination-directory')
+        // let folderStatus = await ipfs.files.stat('/example')
+        // await ipfs.files.write('/example/article.txt',article,{ create: true })
+        // console.log('CID', cid);
+        // console.log('folderStatus', folderStatus);
+        let ls = await ipfs.files.ls('/example')
+        let read = await ipfs.files.read('/article')
+        // await ipfs.files.cp('/example/article.txt', '/destination-directory/article.txt', {parents: true})
+        console.log('all articles', ls)
+        console.log('read', read)
+    }
 
-  function editArticle(CID) {}
+    function getFromIpfs(CID) {
+        //CID string
+    }
 
-  return (
-    <>
-      <input type="text" />
-      <button>Get Article</button>
-      <div>
+    function editArticle(CID) {
+    }
+
+    return (
+        <>
+            <input type="text"
+                   onChange={(e) => {
+                       setState({...state, ['value']: e.target.value})
+                       console.log(state['value'])
+                   }}
+                   placeholder="CID"
+                   value={state['value']}
+            />
+            <button onClick={getArticle}>Get Article</button>
+            <iframe src={state['cid']} width="500" height="500"></iframe>
+            <div>
         <textarea
-          onChange={(e) => {
-            setState({ ...state, ['value']: e.target.value })
-          }}
-          placeholder="CID"
-          value={state['value']}
+            onChange={(e) => {
+                setState({...state, ['article']: e.target.article})
+            }}
+            placeholder="CID"
+            value={state['article']}
         ></textarea>
-        <button onClick={uploadToIpfs}>upload</button>
-      </div>
-      <div>
-        <input
-          style={{ display: 'none' }}
-          ref={inputRef}
-          type="file"
-          onChange={handleFileChange}
-        />
+                <button onClick={uploadToIpfs}>upload</button>
+            </div>
+            <div>
+                <input
+                    style={{display: 'none'}}
+                    ref={inputRef}
+                    type="file"
+                    onChange={handleFileChange}
+                />
 
-        <button onClick={handleClick}>Open file upload box</button>
-      </div>
-    </>
-  )
+                <button onClick={handleClick}>Open file upload box</button>
+            </div>
+        </>
+    )
 }
 
